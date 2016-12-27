1f75850d0363623218b6a21cdccebd199f0ae576
Laurence Elsdon <git@elsdon.me>
2016-12-27 20:38:41 +0000

    Notify of authorisation error

--- src/lib/toggl.js
+++ src/lib/toggl.js
@@ -19,6 +19,8 @@ module.exports = {
         body += chunk
       })
       res.on('end', function() {
+        if(res.statusCode == 403) callback(Error('Request error 403 forbidden. Check \'TOGGL_API_KEY\' env is set'))
+
         var json = JSON.parse(body)
         callback(null, json)
       })
